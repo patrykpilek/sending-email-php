2a9dd1bcf7a9a539a23b8aec9ab9c6eb2b8a3558
Patryk Pilek <patryk.pilek@gmail.com>
2022-12-31 18:39:40 +0000

    install the PHPMailer library with composer

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Content
 
 - [ ] using PHPMailer to send email form PHP
+  - install the PHPMailer library with composer
 - [ ] how to use PHPMailer to send emails
 - [ ] using HTML and CSS to add formatting to your emails
 - [ ] sending emails asynchronously: build a basic asynchronous queue from scratch
--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo 'Hello from the public folder!';
+//echo 'Hello from the public folder!';
+
+/**
+ * PHPMailer Autoloader
+ */
+require '../vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+/**
+ * Check PHPMailer is loaded
+ */
+$mail = new PHPMailer();
+
+echo get_class($mail);
+
