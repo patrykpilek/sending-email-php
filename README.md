31951d269d8606004d81ce54a202a886fecb0c98
Patryk Pilek <patryk.pilek@gmail.com>
2022-12-31 19:01:24 +0000

    configure PHPMailer with your SMTP server

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 - [ ] using PHPMailer to send email form PHP
   - install the PHPMailer library with composer
+  - [Enabling debug output in PHPMailer](https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting#enabling-debug-output)
+  - [Troubleshooting PHPMailer](https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting)
+  - configure PHPMailer with your SMTP server
 - [ ] how to use PHPMailer to send emails
 - [ ] using HTML and CSS to add formatting to your emails
 - [ ] sending emails asynchronously: build a basic asynchronous queue from scratch
--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 
-//echo 'Hello from the public folder!';
-
 /**
  * PHPMailer Autoloader
  */
@@ -10,9 +8,33 @@ require '../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 
 /**
- * Check PHPMailer is loaded
+ * Configure PHPMailer with your SMTP server settings
  */
 $mail = new PHPMailer();
 
-echo get_class($mail);
+$mail->isSMTP();
+$mail->Host = 'mail.example.com';
+$mail->Port = 587;
+$mail->SMTPAuth = true;
+$mail->Username = 'sender@example.com';
+$mail->Password = 'secret';
+$mail->SMTPSecure = 'tls';
+
+/**
+ * Enable SMTP debug messages
+ */
+$mail->SMTPDebug = 2;
+
+/**
+ * Send an email
+ */
+$mail->setFrom('sender@example.com');
+$mail->addAddress('recipient@example.com');
+$mail->Body = 'This is a test message';
+
+if ($mail->send()) {
+    echo 'Message sent!';
+} else {
+    echo 'Mailer error: ' . $mail->ErrorInfo;
+}
 
